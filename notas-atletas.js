1bb815ad4597b5cdc733bd5181d862cac09a3cc3
Wesley <wesley.analistasystem@gmail.com>
2025-12-29 22:06:16 -0300

    Projeto Senai 1

---
+++ notas-atletas.js
@@ -0,0 +1,7 @@
+// aqui vamos desenvolver um programa cálculo de notas para utilizar em eventos esportivos
+
+// nome do aluno, notas[]
+
+// corte da maior nota e corte da menor nota
+
+// calcular a média de notas já excluindo a maior e a menor
