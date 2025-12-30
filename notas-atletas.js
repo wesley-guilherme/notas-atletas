c839c16d943c4bdd0bfb038cf57cee788c4b4f92
wesley-guilherme <wesley.analistasystem@gmail.com>
2025-12-30 15:37:54 -0300

    Primeiro projeto SENAI entregue

--- notas-atletas.js
+++ notas-atletas.js
@@ -5,3 +5,41 @@
 // corte da maior nota e corte da menor nota
 
 // calcular a média de notas já excluindo a maior e a menor
+
+let atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+];
+
+function calcularMedia(atleta) {
+    let ordenadas = [...atleta.notas].sort((a, b) => a - b);
+
+    let notasValidas = ordenadas.slice(1, 4)
+
+    let soma = notasValidas.reduce((acc, nota) => acc + nota, 0);
+    let media = soma / notasValidas.length;
+
+    console.log(`Atleta: ${atleta.nome}`);
+    console.log(`Notas Obtidas: ${atleta.notas.join(",")}`);
+    console.log(`Média Válida: ${media.toLocaleString("pt-BR")}`);
+    console.log("");
+}
+
+atletas.forEach(calcularMedia)
+
+
+
